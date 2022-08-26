7446e53d24ce4a43abe3874dca1ae3787bc67671
halovivek <halovivek@yahoo.com>
2022-08-26 20:12:52 +0530

    List of files from directory

--- 26082002_listfilesfromdirectory.py
+++ 26082002_listfilesfromdirectory.py
@@ -7,5 +7,5 @@ for (root,dirs, file) in os.walk(path):
         print(f)
 
 my_data = pd.DataFrame(file)
-my_data.to_excel("outputfile.xlsx")
+my_data.to_excel("outputfile.xlsx", index = False, header= False)
 
