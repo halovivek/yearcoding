433e07b54e6cc1453cf7ed0d992acf3e64927c90
halovivek <halovivek@gmail.com>
2022-08-29 17:20:15 +0530

    updated

---
+++ 29082022_practice1.py
@@ -0,0 +1,13 @@
+class Player:
+    Team_name = "Team2022"
+    def __init__(self,name,age):
+        self.name = name
+        self.age  = age
+    def display_info(self):
+        return f"The {self.name} player has {self.age} years old"
+
+player01 = Player("a1",25)
+player02 = Player("a2",99)
+
+print(player01.age)
+print(player02.name)
