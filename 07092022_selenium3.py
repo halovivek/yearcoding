c53d5abb0fe228eba425e5de6a857f91ccb3f297
halovivek <halovivek@yahoo.com>
2022-09-08 08:39:44 +0530

    New files added

--- 07092022_selenium3.py
+++ 07092022_selenium3.py
@@ -5,9 +5,9 @@ from selenium.webdriver.chrome.options import Options
 #from selenium.webdriver.support import expected_conditions as EC
 #from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+s = Service(ChromeDriverManager().install())
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-s = Service(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=s)
 #browser = webdriver.Chrome(options=chrome_options)
 browser.get("https://www.google.com")
---
+++ 08092022_selenium1.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.chrome.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
+#from webdriver_manager.chrome import ChromeDriverManager
+s= Service(GeckoDriverManager().install())
+# chrome_options = Options()
+# chrome_options.add_experimental_option("detach",True)
+browser = webdriver.Firefox(service=s)
+#browser = webdriver.Chrome(service=s)
+browser.maximize_window()
+browser.get("https://rahulshettyacademy.com/")
+print(browser.title)
+print(browser.current_url)
+browser.get("https://rahulshettyacademy.com/learning-path")
+browser.back()
+browser.forward()
+browser.minimize_window()
+browser.maximize_window()
+browser.get("https://www.google.com")
+print(browser.current_url)
+#browser.close()
+
+
