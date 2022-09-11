e94c49b88a145a35aae2c206d4459dfb110c2f85
halovivek <halovivek@yahoo.com>
2022-09-11 18:08:11 +0530

    New files added

--- 11092022_selenium1.py
+++ 11092022_selenium1.py
@@ -35,15 +35,16 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 s = Service(ChromeDriverManager().install())
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach",True)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach",True)
+
 """
 The browser will be started.
 """
-browser = webdriver.Chrome(service=s)
+browser = webdriver.Chrome(service=s, options=chrome_options)
 browser.maximize_window()
-chrome_options = Options()
-chrome_options.add_experimental_option("detach",True)
+# chrome_options = Options()
+# chrome_options.add_experimental_option("detach",True)
 browser.get("https://rahulshettyacademy.com")
 print(browser.title)
 browser.get("https://rahulshettyacademy.com/angularpractice/")
