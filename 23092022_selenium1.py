e3d3c87fb15e589dfb4fd1547dc39c28881417b6
halovivek <halovivek@gmail.com>
2022-09-23 17:05:34 +0530

    updated

---
+++ 23092022_selenium1.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+s = Service(ChromeDriverManager().install())
+chrome_options = Options()
+chrome_options.add_experimental_option("detach",True)
+browser = webdriver.Chrome(service=s, options=chrome_options)
+browser.maximize_window()
+browser.implicitly_wait(5)
+
+"""
+Going to use drag and drop 
+get the details from the mouse hover details
+Action is the class which helps to perform
+at the end of the action you need to write perform to execute the things.
+
+"""
+browser.get("https://www.rahulshettyacademy.com/AutomationPractice")
+print(browser.title)
+#//a[normalize-space()='Top']
+#//button[@id='mousehover']
+action = ActionChains(browser)
+action.move_to_element(browser.find_element(*(By.XPATH, "//button[@id='mousehover']"))).perform()
+action.move_to_element(browser.find_element(*(By.LINK_TEXT, "Reload"))).click().perform()
