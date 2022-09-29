64388aec71e40593cd615726fe85a158910d90b8
halovivek <halovivek@yahoo.com>
2022-09-29 08:54:20 +0530

    updates

--- 27092002_selenium3.py
+++ 27092002_selenium3.py
@@ -1,14 +1,14 @@
 import os
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.firefox import GeckoDriver
+#from webdriver_manager.firefox import GeckoDriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options as Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
-
+service = FirefoxService(log_path=os.devnull,)
 options = Options()
 options = webdriver.FirefoxOptions()
 options.binary_location = r"/usr/bin/"
@@ -16,7 +16,7 @@ options.add_argument("start-maximized")
 options.add_experimental_option("detach", True)
 capabilities = webdriver.FirefoxOptions().to_capabilities()
 options.set_capability("cloud:options", capabilities)
-service = FirefoxService(log_path=os.devnull,)
+
 browser = webdriver.Firefox(service= FirefoxService(GeckoDriverManager().install()), options=options)
 #browser = webdriver.Firefox(service=service,options=options,)
 
