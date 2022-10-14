9741fd21b5664873130cce488bc67616b048d623
halovivek <halovivek@yahoo.com>
2022-10-14 20:44:09 +0530

    new file

---
+++ 14102022_practice.py
@@ -0,0 +1,13 @@
+"""
+Practice of Microsoft's Python programming again
+"""
+print("This is test page practice")
+a = 10
+b = 20
+c = a + b
+print(c)
+
+e = "Testing"
+f = "good"
+g = e + f
+print(g)
