c77c711212cf237e4df2e20cdd6789f79e43788c
halovivek <halovivek@yahoo.com>
2022-10-20 08:50:36 +0530

    new file

---
+++ 20102022_python_ifelse.py
@@ -0,0 +1,9 @@
+score = 99
+if score == 100:
+    print("you did it")
+elif score > 90 and  score < 100:
+    print(" you are performer")
+elif score > 80 and score < 89:
+    print("you are doing good")
+else:
+    print("you need to improve")
